1078ccc87adc58d6bc309130f14c91d34c41575f
sy0822-love <dvsssophia@gmail.com>
2026-05-10 16:14:09 +0800

    feat: 實作管理員權限裝飾器

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -3,12 +3,8 @@ from flask import abort
 from flask_login import current_user
 
 def admin_required(f):
-    """
-    權限鎖。
-    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # getattr 防止 current_user 沒有 role 屬性時報錯
         if not current_user.is_authenticated or getattr(current_user, 'role', 0) != 1:
             abort(403)
         return f(*args, **kwargs)
